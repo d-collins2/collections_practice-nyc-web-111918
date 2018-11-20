36e0ae9b8f03df6322d0cd2d23c909b5f75d91ad
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-20 04:11:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort_by do |el| 
+    el.length 
+  end 
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def swap_elements(array)
+  [array[0], array[2], array[1]]
+end 
+
+def kesha_maker(array)
+  array.map do |word|
+    c = word.chars
+    c[2] = "$"
+    c.join    
+  end 
+end 
